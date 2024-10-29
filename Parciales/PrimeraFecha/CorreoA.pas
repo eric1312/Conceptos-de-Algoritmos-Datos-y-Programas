aeaef59dd6682f3b68285c796823aeb1eea9903a
eric1312 <eric.cirimarco@gmail.com>
2024-10-29 00:27:15 -0300

    Ejercicio de correoArg Practica

---
+++ Parciales/PrimeraFecha/CorreoA.pas
@@ -0,0 +1,19 @@
+{La empresa de mensajeria correo Argentino de la ciudad de la Plata le enmendó de informatica
+ un programa para llevar registro de las entregas de sus repartidores. La empresa tiene una lista
+ fisica con 200 repartidores, de los que se conoce DNI, nombre, apellido y edad. A su vez, se dispone
+ de una lista con todos los repartos realizados hasta la fecha, donde cada reparto contiene DNI del repartidor,
+ fecha de entrega, nombre y apellido de la persona que lo recibe, ordenada por DNI del repartidor.
+
+ Se solicita la sig informacion:
+  1. Realizar un modulo que cargue a los empleados en una estructura de datos adecuada.
+  2. Realizar un modulo que recorra la lista de reaprtos y retorne:
+     a) DNI del empleado que mas repartos realizados
+     b) Cantidad de empleados con mas de 10 repartos
+     c) DNI del empleado con menos repartos realizados
+
+Finalmente, muestre en pantalla nombre y apellido del repartidor con mas repartos realizados
+y el que menos repartos realizo, ademas de la cantidad de empleados con mas de 10 repartos.
+}
+
+
+ // escribir el paso a paso aca tambien del ejercicio 
