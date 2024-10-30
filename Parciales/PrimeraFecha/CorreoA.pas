e5fb21e353c7cffe8e540e8f1edf29050082b019
eric1312 <eric.cirimarco@gmail.com>
2024-10-30 00:29:41 -0300

    avance con la resolución del ejercicio

--- Parciales/PrimeraFecha/CorreoA.pas
+++ Parciales/PrimeraFecha/CorreoA.pas
@@ -5,7 +5,7 @@
  fecha de entrega, nombre y apellido de la persona que lo recibe, ordenada por DNI del repartidor.
 
  Se solicita la sig informacion:
-  1. Realizar un modulo que cargue a los empleados en una estructura de datos adecuada.
+  1. Realizar un modulo que cargue a los empleados en una estructura de datos adecuada y la retorne.
   2. Realizar un modulo que recorra la lista de reaprtos y retorne:
      a) DNI del empleado que mas repartos realizados
      b) Cantidad de empleados con mas de 10 repartos
@@ -17,3 +17,103 @@ y el que menos repartos realizo, ademas de la cantidad de empleados con mas de 1
 
 
  // escribir el paso a paso aca tambien del ejercicio 
+
+         //La empresa tiene una lista
+         //fisica con 200 repartidores, de los que se conoce DNI, nombre, apellido y edad.
+
+         {debemos representar 200 repartidores ¿lista o arreglo? ¿de que tipo?
+               Es un arreglo y de un tipo de registro
+
+         ¿Como represento a cada repartidor?}
+
+
+         {A su vez, se dispone de una lista con todos los repartos realizados hasta la fecha, 
+            donde cada reparto contiene DNI del repartidor, fecha de entrega, nombre y apellido 
+            de la persona que lo recibe, ordenada por DNI del repartidor (registro entrega)
+            
+            
+            se DISPONE, dato interesante...
+            
+            
+            nos habla de una lista, sabemos representarla ¿que tiene de interesante la misma?
+                     La lista se encuentra ordenada por DNI del repartidor 
+                     A su vez, es un corte de control
+                     
+                     
+            
+            
+            1. Realizar un modulo que cargue a los empleados en una 
+            
+               estructura de datos adecuada y la retorne.
+               
+               
+               Ya charlamos sobre esto¿se hacen una idea de como hacerlos?
+                        .Es agregar elementos en un vector}
+
+
+
+            {2. Realizar un modulo que recorra la lista de reaprtos y retorne:
+                a) DNI del empleado que mas repartos realizados
+                b) Cantidad de empleados con mas de 10 repartos
+                c) DNI del empleado con menos repartos realizados
+                
+                
+                debemos recorrer la lista ¿hay qye utilizar algun algoritmo que ya conozcan?
+                  Con While}
+
+
+            {El corte de control trabaja en grupo y siempre con el mismo numero o lo que sea, siempre va a 
+            estar ordenada.}
+
+Program Mensajeria;
+const
+   Max_repartidores = 200;
+type
+   Repartidor = record
+      DNI:integer;
+      nombre, apellido:string [40];
+      edad:integer;
+   end;
+   
+   vectorRepartidores = array [1..Max_repartidores] of repartidor;
+
+   Entrega = record 
+      dniRepartidor:integer;
+      fechaDeEntrega:string [30];
+      nombreReceptor, apellidoReceptor:string[50];
+   end;
+
+   ListaRepartos = ^Nodo;
+
+   Nodo = record 
+      dato:Entrega;
+      sig: ListaRepartos;
+   end;
+
+//cargando un vector 
+procedure cargarVector(var vectorRepartidor:vectorRepartidores);
+var  
+   i:integer;
+   repartidor:Repartidor;
+begin
+  for i := 1 to Max_repartidores do begin
+      leerRepartidor(repartidor);
+      vectorRepartidor[i]:=repartidor;
+   end;
+end;
+
+//leer los datos del repartidro por teclado
+procedure leerRepartidor (var repartidor:Repartidor);
+begin
+   writeln ("ingrese el nombre del repartidor:")
+   readln (repartidor.nombre);
+
+   writeln ("ingrese el apellido del repartidor:")
+   readln (repartidor.apellido);
+
+   writeln ("ingrese el DNI del repartidor:")
+   readln (repartidor.dni);
+
+   writeln ("ingrese el edad del repartidor:")
+   readln (repartidor.edad);
+end;
